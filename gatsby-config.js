4f2acce673b558224a74dc77329875c0c258585e
Goudmane Oualid <walidlioon.09@gmail.com>
2022-11-29 03:45:12 +0100

    b

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Goudmane Oualid',
     description:
       'Goudmane Oualid is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'http://localhost:9000/', // No trailing slash allowed!
+    siteUrl: 'https://majestic-bienenstitch-60a7c0.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
