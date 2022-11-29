ac0345f5e052f45a0d07d8738bd0ecaeca346152
Goudmane Oualid <walidlioon.09@gmail.com>
2022-11-29 08:49:55 +0100

    vv

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Goudmane Oualid',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Goudmane Oualid is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: '', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Goudmane Oualid',
+        short_name: 'Goudmane Oualid',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        //icon: 'src/images/logo.png',
       },
     },
     `gatsby-plugin-offline`,
